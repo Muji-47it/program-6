dde56335f904fa390e608ec647c461361923d72b
Muji-47it <107984150+Muji-47it@users.noreply.github.com>
2022-06-24 16:37:32 +0500

    Add files via upload

---
+++ arrayassignment.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,3,4,5};
+    int max;
+    max=arr[0];
+    for (int i = 0; i < 5; i++)
+    {
+        if(arr[i]>max)
+        max=arr[i];
+    }
+    cout<<max;   
+}
